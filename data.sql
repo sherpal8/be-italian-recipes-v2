0a14459da20338df87ab62f2101a5b6a372174fb
sherpal <sherpalkorotana@icloud.com>
2019-09-16 08:03:41 +0100

    basic setup

--- data.sql
+++ data.sql
@@ -1,1 +1,23 @@
--- your sql code
+DROP DATABASE IF EXISTS italianDB;
+CREATE DATABASE italianDB;
+
+\c italianDB
+
+CREATE TABLE course
+(
+course_id SERIAL PRIMARY KEY,
+course_type VARCHAR 
+);
+
+INSERT INTO course (course_type)
+VALUES ('Starter'),
+       ('Main') ,
+       ('Dessert'),
+       ('Drink');
+
+\dt
+
+SELECT * FROM course;
+
+
+
